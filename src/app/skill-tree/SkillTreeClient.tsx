e03ab915b4dec34178f0085bb28fc6a9a011c438
anthony <hi.anthonyyang@gmail.com>
2025-08-09 23:33:28 -0700

    Chore: remove legacy skill-tree page and pages/api/careers/*; update nav later to hide link (will adjust separately).

--- src/app/skill-tree/SkillTreeClient.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const SkillTree = dynamic(() => import("./SkillTree"), { ssr: false });
-
-export default function SkillTreeClient() {
-  return <SkillTree />;
-} 
